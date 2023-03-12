6e86dd28ddea94c7a8114302444988d2f40e287b
DawidNst <dawidniesterczuk@gmail.com>
2023-03-12 18:53:08 +0100

    change

---
+++ src/main/java/Rest/Api/model/TaskRepository.java
@@ -0,0 +1,11 @@
+package Rest.Api.model;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource
+interface TaskRepository extends JpaRepository {
+
+
+}
